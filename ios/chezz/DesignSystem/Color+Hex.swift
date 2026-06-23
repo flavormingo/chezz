8d3d6747c75f120b2f49c5b5b831d556f06a7db9
flavormingo <me@mazz.tv>
2026-06-23 01:59:10 -0500

    I will not cut corners.

---
+++ ios/chezz/DesignSystem/Color+Hex.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let s = hex.trimmingCharacters(in: CharacterSet(charactersIn: "#"))
+        var rgb: UInt64 = 0
+        Scanner(string: s).scanHexInt64(&rgb)
+        let r, g, b, a: Double
+        switch s.count {
+        case 3:
+            r = Double((rgb >> 8) & 0xF) / 15.0
+            g = Double((rgb >> 4) & 0xF) / 15.0
+            b = Double(rgb & 0xF) / 15.0
+            a = 1.0
+        case 8:
+            r = Double((rgb >> 24) & 0xFF) / 255.0
+            g = Double((rgb >> 16) & 0xFF) / 255.0
+            b = Double((rgb >> 8) & 0xFF) / 255.0
+            a = Double(rgb & 0xFF) / 255.0
+        default:
+            r = Double((rgb >> 16) & 0xFF) / 255.0
+            g = Double((rgb >> 8) & 0xFF) / 255.0
+            b = Double(rgb & 0xFF) / 255.0
+            a = 1.0
+        }
+        self.init(.sRGB, red: r, green: g, blue: b, opacity: a)
+    }
+}
